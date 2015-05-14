712dd298fa4570f5dd2f81adc3fae014db565d88
masx <migue.masx@gmail.com>
2015-05-14 19:04:57 +0200

    two models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Exam.create([{description:'FUNDAMENTOS FÍSICOS
+DE LA INFORMÁTICA', date:Date.today}])
