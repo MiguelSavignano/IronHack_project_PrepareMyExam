fb428b34728d1aaeda5929f89001df50280f62cd
masx <migue.masx@gmail.com>
2015-05-21 17:57:52 +0200

    css style add method sum themes in model exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,12 @@
 class Exam < ActiveRecord::Base
 	belongs_to :subject
 	has_many :exam_themes
-	has_many :study_sessions
 	has_many :themes, through: :exam_themes
+	has_many :study_sessions
+
+def sum_themes
+	self.themes.reduce(0){|sum,theme|sum+theme.minutes}
 end
+
+end
+
