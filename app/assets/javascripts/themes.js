6dfaa2b8d3b89965089a5c0772da177669997402
masx <migue.masx@gmail.com>
2015-05-28 03:29:19 +0200

    script in tags

--- app/assets/javascripts/themes.js
+++ app/assets/javascripts/themes.js
@@ -1,15 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-      [5, 2, 4, 2, 0]
-    ]
-  };
-  
-  // Create a new line chart object where as first parameter we pass in a selector
-  // that is resolving to our chart container element. The Second parameter
-  // is the actual data object.
-  new Chartist.Line('.ct-chart', data);
