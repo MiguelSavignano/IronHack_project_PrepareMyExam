47c778fee2e298caa544a8f7d37cae65fface6d0
masx <migue.masx@gmail.com>
2015-05-29 10:44:01 +0200

    refactor home jsonbuilder

--- app/views/users/home.json.jbuilder
+++ app/views/users/home.json.jbuilder
@@ -1,2 +1,4 @@
 json.study_percent  @study_percent
-json.study_sessions  @study_sessions.map{ |study_session| study_session.minutes}
+json.study_sessions(@study_sessions) do |study_sessions|
+  json.extract! study_sessions, :id, :minutes
+end
--- public/assets/js/draw_chartist_line_study_sessions.js
+++ public/assets/js/draw_chartist_line_study_sessions.js
@@ -1,7 +1,7 @@
 function draw_chartist_line_study_sessions(study_sessions){
   var  study_sessions_minutes =  study_sessions.map(function(study_session){return  study_session.minutes;});
   var  study_sessions_labels_blank =  study_sessions.map(function(study_session){return  "";});
-   new Chartist.Line('.ct-chart', {
+   new Chartist.Line('#chart_line', {
      labels: study_sessions_labels_blank,
      series: [
        study_sessions_minutes
